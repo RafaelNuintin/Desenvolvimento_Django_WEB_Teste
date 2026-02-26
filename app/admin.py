e965aa7523a4efe8c45268f96a215b57f7e851a6
RafaelNuintin <rafael.nuintin@alunos.ifsuldeminas.edu.br>
2026-02-26 12:18:49 -0300

    commit 26-02-2026

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from .models import *
-from django.contrib import admin
+
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1
+
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', )
+    search_fields = ('nome', )
+    inlines = [LivroInline]
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', )
+    search_fields = ('nome',)
+    inlines = [LivroInline]
 
 admin.site.register(Cidade)
-admin.site.register(Autor)
-admin.site.register(Editora)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Editora, EditoraAdmin)
 admin.site.register(Leitor)
 admin.site.register(Livro)
 admin.site.register(Genero)
